dbfb472d374f62d8c8be28c2c4137eaa9d419901
ms_udee <u.uduakdavid@gmail.com>
2023-08-01 15:39:14 +0100

    Blog Post

--- js/blog.js
+++ js/blog.js
@@ -1,24 +1,36 @@
-const main = document.querySelector('main');
+const body = document.querySelector("body");
+const main = document.querySelector("main");
 let baseUrl = `https://jsonplaceholder.typicode.com/posts`;
-let buttons = document.querySelector('button');
-fetch(baseUrl).then(function (res) {
-    return res.json()
-}).then(function (data) {
+// const buttons = document.querySelectorAll("button");
+// const div = document.querySelectorAll("div");
+// const h2 = document.querySelectorAll("h2");
+// const p = document.getElementsByTagName("p");
 
-    let section = ''
+fetch(baseUrl)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    let section = "";
 
     data.forEach(function (data) {
-        let { title, body } = data;
-        section += `   
+      let { title, body } = data;
+      section += `   
     <div>
-      <h4>${title}</h4>
+      <h2>${title}</h2>
       <p>${body.substring(0, 80)}...</p>
       <button>read more</button>
     </div>
-    `
-        main.innerHTML = section;
-        
-    });
-});
 
-   
+    
+    `;
+
+      main.innerHTML = section;
+    });
+  });
+
+// div.classList.add("container");
+// buttons.classList.add("btn");
+// h2.classList.add("heading");
+// p.classList.add("para");
+// console.log(body);
